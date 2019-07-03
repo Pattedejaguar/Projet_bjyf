a9af1e15bfd4960428a0c8dc9ed299da1df30e78
xxBeloule <trezaaqq@hotmail.fr>
2019-07-03 14:43:38 +0200

    Navbar

--- index.php
+++ index.php
@@ -1,53 +1,47 @@
 <!DOCTYPE html>
 <html lang="fr">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <link rel="stylesheet" href="assets/css/bootstrap.min.css">
-        <link rel="stylesheet" href="assets/css/style.css">
-        <title>projet-php</title>
-    </head>
-    <body>
-        <?php
-        $fichier = 'source.xml'; //recupération du ficher dans une variable
-        $xml = simplexml_load_file($fichier); //chargement du fichier
-        foreach ($xml as $pages) {
-            echo $pages->menu;
-            echo $pages->title;
-            echo $pages->content;
-        }
-        $xml = simplexml_load_file('source.xml');
-        $id = $_GET['id'];
-        foreach ($xml as $pages){
-        ?>
-        <a href="index.php?id=<?= $pages['id'] ?>"><?= $pages->menu ?></a>
-    <?php } ?>
+<head>
+  <meta charset="utf-8">
+  <title>Maçonnerie Ocordo</title>
+<link href="https://stackpath.bootstrapcdn.com/bootswatch/4.3.1/darkly/bootstrap.min.css" rel="stylesheet" integrity="sha384-w+8Gqjk9Cuo6XH9HKHG5t5I1VR4YBNdPt/29vwgfZR485eoEJZ8rJRbm3TR32P6k" crossorigin="anonymous">
+<link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+  <div class="container">
+  <nav class="nav justify-content-center">
     <?php
-    echo '<br>';
-    switch ($id) {
-        case 1:
-            echo $xml->page[0]->title;
-            echo $xml->page[0]->content;
-            break;
-        case 2:
-            echo $xml->page[1]->title;
-            echo $xml->page[1]->content;
-            break;
-        case 3:
-            echo $xml->page[2]->title;
-            echo $xml->page[2]->content;
-            break;
-        case 4:
-            echo $xml->page[3]->title;
-            echo $xml->page[3]->content;
-            break;
-        default:
-            echo $xml->page[0]->title;
-            echo $xml->page[0]->content;
-            break;
-    }
-    ?>
-
-    </body>
+  $fichier = 'source.xml'; //recupération du ficher dans une variable
+  $xml = simplexml_load_file($fichier);
+                $id = $_GET['id'];
+                foreach ($xml as $pages){
+              ?>
+              <a class ="nav-link" href="index.php?id=<?= $pages['id'] ?>"><?= $pages->menu ?></a>
+              <?php } ?>
+            </nav>
+            <?php
+              echo '<br>';
+              switch ($id) {
+                  case 1:
+                      echo $xml->page[0]->title;
+                      echo $xml->page[0]->content;
+                      break;
+                  case 2:
+                      echo $xml->page[1]->title;
+                      echo $xml->page[1]->content;
+                      break;
+                  case 3:
+                      echo $xml->page[2]->title;
+                      echo $xml->page[2]->content;
+                      break;
+                  case 4:
+                      echo $xml->page[3]->title;
+                      echo $xml->page[3]->content;
+                      break;
+                  default:
+                      echo $xml->page[0]->title;
+                      echo $xml->page[0]->content;
+                      break;
+              } ?>
+              </div>
+</body>
 </html>
